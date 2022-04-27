b521e8e6b766d81b72a40327dd8f22ed0a8e66f3
Prakhar-mehrishi <62169851+Prakhar-mehrishi@users.noreply.github.com>
2022-04-27 09:49:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,149 +1,109 @@
-# App created by Data Professor http://youtube.com/dataprofessor
-# GitHub repo of this app https://github.com/dataprofessor/ml-auto-app
-# Demo of this app https://share.streamlit.io/dataprofessor/ml-auto-app/main/app.py
-
 import streamlit as st
 import pandas as pd
-from lazypredict.Supervised import LazyRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.datasets import load_diabetes, load_boston
-import matplotlib.pyplot as plt
-import seaborn as sns
-import base64
-import io
-#---------------------------------#
+
+# ---------------------------------#
 # Page layout
 ## Page expands to full width
-st.set_page_config(page_title='The Machine Learning Algorithm Comparison App',
-    layout='wide')
-#---------------------------------#
+st.set_page_config(page_title='The Auto Machine Learning App',
+                   layout='wide')
+
+
+# ---------------------------------#
 # Model building
 def build_model(df):
-    df = df.loc[:100] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
-    X = df.iloc[:,:-1] # Using all column except for the last column as X
-    Y = df.iloc[:,-1] # Selecting the last column as Y
+    X = df.iloc[:, :-1]  # Using all column except for the last column as X
+    Y = df.iloc[:, -1]  # Selecting the last column as Y
 
-    st.markdown('**1.2. Dataset dimension**')
-    st.write('X')
-    st.info(X.shape)
-    st.write('Y')
-    st.info(Y.shape)
+    # Data splitting
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=(100 - split_size) / 100)
+
+    st.markdown('**1.2. Data splits**')
+    st.write('Training set')
+    st.info(X_train.shape)
+    st.write('Test set')
+    st.info(X_test.shape)
 
     st.markdown('**1.3. Variable details**:')
-    st.write('X variable (first 20 are shown)')
-    st.info(list(X.columns[:20]))
+    st.write('X variable')
+    st.info(list(X.columns))
     st.write('Y variable')
     st.info(Y.name)
 
-    # Build lazy model
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y,test_size = split_size,random_state = seed_number)
-    reg = LazyRegressor(verbose=0,ignore_warnings=False, custom_metric=None)
-    models_train,predictions_train = reg.fit(X_train, X_train, Y_train, Y_train)
-    models_test,predictions_test = reg.fit(X_train, X_test, Y_train, Y_test)
+    rf = RandomForestRegressor(n_estimators=parameter_n_estimators,
+                               random_state=parameter_random_state,
+                               max_features=parameter_max_features,
+                               criterion=parameter_criterion,
+                               min_samples_split=parameter_min_samples_split,
+                               min_samples_leaf=parameter_min_samples_leaf,
+                               bootstrap=parameter_bootstrap,
+                               oob_score=parameter_oob_score,
+                               n_jobs=parameter_n_jobs)
+    rf.fit(X_train, Y_train)
 
-    st.subheader('2. Table of Model Performance')
+    st.subheader('2. Model Performance')
 
-    st.write('Training set')
-    st.write(predictions_train)
-    st.markdown(filedownload(predictions_train,'training.csv'), unsafe_allow_html=True)
+    st.markdown('**2.1. Training set**')
+    Y_pred_train = rf.predict(X_train)
+    st.write('Coefficient of determination ($R^2$):')
+    st.info(r2_score(Y_train, Y_pred_train))
 
-    st.write('Test set')
-    st.write(predictions_test)
-    st.markdown(filedownload(predictions_test,'test.csv'), unsafe_allow_html=True)
+    st.write('Error (MSE or MAE):')
+    st.info(mean_squared_error(Y_train, Y_pred_train))
 
-    st.subheader('3. Plot of Model Performance (Test set)')
+    st.markdown('**2.2. Test set**')
+    Y_pred_test = rf.predict(X_test)
+    st.write('Coefficient of determination ($R^2$):')
+    st.info(r2_score(Y_test, Y_pred_test))
+
+    st.write('Error (MSE or MAE):')
+    st.info(mean_squared_error(Y_test, Y_pred_test))
+
+    st.subheader('3. Model Parameters')
+    st.write(rf.get_params())
 
 
-    with st.markdown('**R-squared**'):
-        # Tall
-        predictions_test["R-Squared"] = [0 if i < 0 else i for i in predictions_test["R-Squared"] ]
-        plt.figure(figsize=(3, 9))
-        sns.set_theme(style="whitegrid")
-        ax1 = sns.barplot(y=predictions_test.index, x="R-Squared", data=predictions_test)
-        ax1.set(xlim=(0, 1))
-    st.markdown(imagedownload(plt,'plot-r2-tall.pdf'), unsafe_allow_html=True)
-        # Wide
-    plt.figure(figsize=(9, 3))
-    sns.set_theme(style="whitegrid")
-    ax1 = sns.barplot(x=predictions_test.index, y="R-Squared", data=predictions_test)
-    ax1.set(ylim=(0, 1))
-    plt.xticks(rotation=90)
-    st.pyplot(plt)
-    st.markdown(imagedownload(plt,'plot-r2-wide.pdf'), unsafe_allow_html=True)
-
-    with st.markdown('**RMSE (capped at 50)**'):
-        # Tall
-        predictions_test["RMSE"] = [50 if i > 50 else i for i in predictions_test["RMSE"] ]
-        plt.figure(figsize=(3, 9))
-        sns.set_theme(style="whitegrid")
-        ax2 = sns.barplot(y=predictions_test.index, x="RMSE", data=predictions_test)
-    st.markdown(imagedownload(plt,'plot-rmse-tall.pdf'), unsafe_allow_html=True)
-        # Wide
-    plt.figure(figsize=(9, 3))
-    sns.set_theme(style="whitegrid")
-    ax2 = sns.barplot(x=predictions_test.index, y="RMSE", data=predictions_test)
-    plt.xticks(rotation=90)
-    st.pyplot(plt)
-    st.markdown(imagedownload(plt,'plot-rmse-wide.pdf'), unsafe_allow_html=True)
-
-    with st.markdown('**Calculation time**'):
-        # Tall
-        predictions_test["Time Taken"] = [0 if i < 0 else i for i in predictions_test["Time Taken"] ]
-        plt.figure(figsize=(3, 9))
-        sns.set_theme(style="whitegrid")
-        ax3 = sns.barplot(y=predictions_test.index, x="Time Taken", data=predictions_test)
-    st.markdown(imagedownload(plt,'plot-calculation-time-tall.pdf'), unsafe_allow_html=True)
-        # Wide
-    plt.figure(figsize=(9, 3))
-    sns.set_theme(style="whitegrid")
-    ax3 = sns.barplot(x=predictions_test.index, y="Time Taken", data=predictions_test)
-    plt.xticks(rotation=90)
-    st.pyplot(plt)
-    st.markdown(imagedownload(plt,'plot-calculation-time-wide.pdf'), unsafe_allow_html=True)
-
-# Download CSV data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-def filedownload(df, filename):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download={filename}>Download {filename} File</a>'
-    return href
-
-def imagedownload(plt, filename):
-    s = io.BytesIO()
-    plt.savefig(s, format='pdf', bbox_inches='tight')
-    plt.close()
-    b64 = base64.b64encode(s.getvalue()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:image/png;base64,{b64}" download={filename}>Download {filename} File</a>'
-    return href
-
-#---------------------------------#
+# ---------------------------------#
 st.write("""
-# The Machine Learning Algorithm Comparison App
+# The Auto Machine Learning App
 
-In this implementation, the **lazypredict** library is used for building several machine learning models at once.
-
-Developed by: [Data Professor](http://youtube.com/dataprofessor)
+Try adjusting the hyperparameters!
 
 """)
 
-#---------------------------------#
+# ---------------------------------#
 # Sidebar - Collects user input features into dataframe
 with st.sidebar.header('1. Upload your CSV data'):
     uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
     st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+[Example CSV input file](https://github.com/Prakhar-mehrishi/Auto-Ml/blob/main/delaney_solubility_with_descriptors.csv)
 """)
 
 # Sidebar - Specify parameter settings
 with st.sidebar.header('2. Set Parameters'):
     split_size = st.sidebar.slider('Data split ratio (% for Training Set)', 10, 90, 80, 5)
-    seed_number = st.sidebar.slider('Set the random seed number', 1, 100, 42, 1)
 
+with st.sidebar.subheader('2.1. Learning Parameters'):
+    parameter_n_estimators = st.sidebar.slider('Number of estimators (n_estimators)', 0, 1000, 100, 100)
+    parameter_max_features = st.sidebar.select_slider('Max features (max_features)', options=['auto', 'sqrt', 'log2'])
+    parameter_min_samples_split = st.sidebar.slider(
+        'Minimum number of samples required to split an internal node (min_samples_split)', 1, 10, 2, 1)
+    parameter_min_samples_leaf = st.sidebar.slider(
+        'Minimum number of samples required to be at a leaf node (min_samples_leaf)', 1, 10, 2, 1)
 
-#---------------------------------#
+with st.sidebar.subheader('2.2. General Parameters'):
+    parameter_random_state = st.sidebar.slider('Seed number (random_state)', 0, 1000, 42, 1)
+    parameter_criterion = st.sidebar.select_slider('Performance measure (criterion)', options=['mse', 'mae'])
+    parameter_bootstrap = st.sidebar.select_slider('Bootstrap samples when building trees (bootstrap)',
+                                                   options=[True, False])
+    parameter_oob_score = st.sidebar.select_slider(
+        'Whether to use out-of-bag samples to estimate the R^2 on unseen data (oob_score)', options=[False, True])
+    parameter_n_jobs = st.sidebar.select_slider('Number of jobs to run in parallel (n_jobs)', options=[1, -1])
+
+# ---------------------------------#
 # Main panel
 
 # Displays the dataset
@@ -158,21 +118,19 @@ else:
     st.info('Awaiting for CSV file to be uploaded.')
     if st.button('Press to use Example Dataset'):
         # Diabetes dataset
-        #diabetes = load_diabetes()
-        #X = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
-        #Y = pd.Series(diabetes.target, name='response')
-        #df = pd.concat( [X,Y], axis=1 )
+        # diabetes = load_diabetes()
+        # X = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
+        # Y = pd.Series(diabetes.target, name='response')
+        # df = pd.concat( [X,Y], axis=1 )
 
-        #st.markdown('The Diabetes dataset is used as the example.')
-        #st.write(df.head(5))
+        # st.markdown('The Diabetes dataset is used as the example.')
+        # st.write(df.head(5))
 
         # Boston housing dataset
         boston = load_boston()
-        #X = pd.DataFrame(boston.data, columns=boston.feature_names)
-        #Y = pd.Series(boston.target, name='response')
-        X = pd.DataFrame(boston.data, columns=boston.feature_names).loc[:100] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
-        Y = pd.Series(boston.target, name='response').loc[:100] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
-        df = pd.concat( [X,Y], axis=1 )
+        X = pd.DataFrame(boston.data, columns=boston.feature_names)
+        Y = pd.Series(boston.target, name='response')
+        df = pd.concat([X, Y], axis=1)
 
         st.markdown('The Boston housing dataset is used as the example.')
         st.write(df.head(5))
